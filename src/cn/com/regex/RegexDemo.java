e178ee013e63b461d54d20c7e3e0c585862d71ea
FHtengjiao <fhxtj1991@163.com>
2018-05-09 14:55:14 +0800

    提交新代码

--- src/cn/com/regex/RegexDemo.java
+++ src/cn/com/regex/RegexDemo.java
@@ -1,4 +1,14 @@
 package cn.com.regex;
 
 public class RegexDemo {
+    public static void main(String[] args) {
+        splitString("c:\\me\\abc.txt", "\\\\");
+    }
+
+    public static void splitString(String str, String regex){
+        String[] arr = str.split(regex);
+        for (String s: arr){
+            System.out.println(s);
+        }
+    }
 }
