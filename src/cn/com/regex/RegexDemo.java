73741aac6617b437ee39b7e83d37ea2c5a3e56b3
FHtengjiao <fhxtj1991@163.com>
2018-05-09 15:04:54 +0800

    提交新代码

--- src/cn/com/regex/RegexDemo.java
+++ src/cn/com/regex/RegexDemo.java
@@ -3,13 +3,18 @@ package cn.com.regex;
 public class RegexDemo {
     public static void main(String[] args) {
         //splitString("c:\\me\\abc.txt", "\\\\");
-        splitString("annnnmnnnnnjskkue,,kj  hyjjjdssdfa..iyre", "(.)\\1+");
+        replaceString("annnnmnnnnnjskkue,,kj  hyjjjdssdfa..iyre", "(.)\\1+", "$1");
     }
 
     public static void splitString(String str, String regex){
         String[] arr = str.split(regex);
         for (String s: arr){
-            System.out.println(s);
+                System.out.println(s);
         }
+    }
+
+    public static void replaceString(String str, String regex, String newStr){
+        str = str.replaceAll(regex, newStr);
+        System.out.println(str);
     }
 }
