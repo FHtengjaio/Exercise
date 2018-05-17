2eeb98bfdafd5c59ff6b6532f1dbed9a759326d1
FHtengjiao <fhxtj1991@163.com>
2018-05-17 14:01:27 +0800

    Collection demo exercise

--- src/cn/com/collection/CollectionDemo.java
+++ src/cn/com/collection/CollectionDemo.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class CollectionDemo {
 
     public static void main(String[] args) {
-        hashmapSort2();
+        linkedHashMapDemo();
     }
 
     private static void hashmapSort2() {
