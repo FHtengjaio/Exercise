888d59f4e319fb286c29c1cfec242845420f25d7
tengjiao <35208845+FHtengjaio@users.noreply.github.com>
2018-05-17 09:34:10 +0800

    Delete DownThread.java

--- src/cn/com/netdown/DownThread.java
+++
@@ -1,9 +0,0 @@
-package cn.com.netdown;
-
-import java.io.RandomAccessFile;
-
-public class DownThread {
-
-    public DownThread(int startPos, int currentFileSize, RandomAccessFile currentPart) {
-    }
-}
