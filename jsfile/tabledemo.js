822eb8c9f9e535ea41bd0a2c63478b6e12994993
Tengjiao <tester@test.com>
2018-05-23 21:54:37 +0800

    submit new code

--- jsfile/tabledemo.js
+++ jsfile/tabledemo.js
@@ -2,7 +2,6 @@ var name;
 onload = function (ev) {
     var oTabNode = document.getElementById("tabid");
     var oTrNodes = oTabNode.rows;
-    var oTdNodes;
     for(var i=1; i<oTrNodes.length;i++){
         if(i % 2){
             oTrNodes[i].className = "odd";
@@ -20,3 +19,45 @@ onload = function (ev) {
     }
 
 };
+var flag = true;
+function sortTab() {
+    var oTabNode = document.getElementById("tabid");
+    var oTrNodes = oTabNode.rows;
+
+    if(flag){
+        swap(oTrNodes);
+        flag = false;
+    }else{
+        swap1(oTrNodes);
+        flag = true;
+    }
+}
+
+function swap(arr) {
+    var collTempTrs = new Array(3);
+    for(var i=1; i<arr.length-1; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i].cells[1].innerHTML > arr[j].cells[1].innerHTML) {
+                for (var k = 0; k < arr[i].cells.length; k++) {
+                    collTempTrs[k] = arr[i].cells[k].innerHTML;
+                    arr[i].cells[k].innerHTML = arr[j].cells[k].innerHTML;
+                    arr[j].cells[k].innerHTML = collTempTrs[k];
+                }
+            }
+        }
+    }
+}
+function swap1(arr) {
+    var collTempTrs = new Array(3);
+    for(var i=1; i<arr.length-1; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i].cells[1].innerHTML < arr[j].cells[1].innerHTML) {
+                for (var k = 0; k < arr[i].cells.length; k++) {
+                    collTempTrs[k] = arr[i].cells[k].innerHTML;
+                    arr[i].cells[k].innerHTML = arr[j].cells[k].innerHTML;
+                    arr[j].cells[k].innerHTML = collTempTrs[k];
+                }
+            }
+        }
+    }
+}
